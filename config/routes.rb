64cc6f627f9587948b9b294e505eb34148211c5c
Alex Arwine <alex.arwine@thrivemarket.com>
2017-06-20 16:40:48 -0700

    Add docker environment/bits to ease development

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,15 @@ Rails.application.routes.draw do
 
   get '/dashboard' => 'dashboard#show', as: :dashboard
   root to: 'dashboard#show'
+
+  scope '/api' do
+    scope '/v1' do
+      scope '/players' do
+        post '/create' => 'api_players#create'
+      end
+      scope '/games' do
+        post '/:game_id/results/create' => 'api_results#create'
+      end
+    end
+  end
 end
