6d8f4dfdfe8a199e3d6db6aa624fa3f18d131534
Alex Arwine <alex.arwine@thrivemarket.com>
2017-06-22 16:04:07 -0700

    Add json for ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,14 @@
 class RatingsController < ApplicationController
   def index
     @game = Game.find(params[:game_id])
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @game
+      end
+    end
+  end
+
+  def show
   end
 end
