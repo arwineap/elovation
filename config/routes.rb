f496517b4bdc9b9eb54ebbe120c10ae3ee3211e6
Alex Arwine <alex.arwine@thrivemarket.com>
2017-06-21 17:54:57 -0700

    adding routes for api palyer games

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,11 @@ Rails.application.routes.draw do
   scope '/api' do
     scope '/v1' do
       scope '/players' do
-        post '/create'  => 'api_players#create'
-        get  '/:id/'    => 'api_players#show'
-        post  '/email/' => 'api_players#get_by_email'
-        post  '/name/'  => 'api_players#get_by_name'
+        post '/create'               => 'api_players#create'
+        get  '/:id/'                 => 'api_players#show'
+        post  '/email/'              => 'api_players#get_by_email'
+        post  '/name/'               => 'api_players#get_by_name'
+        get  '/:player_id/games/:id' => 'api_player_games#show'
       end
       scope '/games' do
         post '/:game_id/results/create' => 'api_results#create'
